8ca6cd145676162ed4ae0521ad936c40e508dbe8
jk.jayakrishnan27 <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2024-03-09 01:11:48 +0530

    implement viewmodel

---
+++ app/src/main/java/com/example/retrofit_demo/viewmodel/CharacterViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.retrofit_demo.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.retrofit_demo.repository.CharacterRepo
+import com.example.retrofit_demo.retrofit.Character
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class CharacterViewModel(val characterRepo : CharacterRepo) : ViewModel() {
+    private val _state = MutableStateFlow(emptyList<Character>())
+    val state : StateFlow<List<Character>>
+        get() = _state
+
+    init {
+        viewModelScope.launch {
+            val characters = characterRepo.getCharacters()
+            _state.value = characters
+        }
+    }
+}
